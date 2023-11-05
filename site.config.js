503b01982cea823ab66dd0b9fca2701fa077b276
changed2 <128599878+changed2@users.noreply.github.com>
2023-11-06 00:30:12 +1100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "edo",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/ed-avat.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Student",
     bio: "Something Awesome",
     email: "",
