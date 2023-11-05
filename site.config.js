11fa3820d758d975000852214a219f91f38dad0b
changed2 <128599878+changed2@users.noreply.github.com>
2023-11-06 00:28:08 +1100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "edo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Student",
+    bio: "Something Awesome",
+    email: "",
+    linkedin: "",
+    github: "changed2",
+    instagram: "uos.ip3",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/changed2/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Awesome Something",
+    description: "welcome to my something awesome!",
   },
 
   // CONFIG configration (required)
